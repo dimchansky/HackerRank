978a72c46aa469faad0d7e44c8e05c19593d8348
Dmitrij Koniajev <dimchansky@gmail.com>
2014-07-10 01:16:18 +0300

    Added solution for "Manasa and Stones"

---
+++ Algorithms/Warmup/ManasaAndStones/src/Main.hs
@@ -0,0 +1,16 @@
+module Main where
+
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (replicateM)
+
+possibleValuesForLastStone :: Integer -> Integer -> Integer -> [Integer]
+possibleValuesForLastStone n a b
+    | a == b     = [(n - 1) * a] 
+    | a >= b     = [na * a + (n - 1 - na) * b | na <- [0 .. n-1]]
+    | otherwise = possibleValuesForLastStone n b a     
+
+main :: IO ()
+main = 
+    readLn 
+    >>= flip replicateM (possibleValuesForLastStone <$> readLn <*> readLn <*> readLn)
+    >>= mapM_ (putStrLn . unwords . map show)
